72ccb735bd32c5462e8161eb83dd2b7cd4b94b36
BestAutoyes <enrico-eifert@t-online.de>
2022-04-03 16:08:00 +0200

    players will be a spectator when joining too late or when dying

--- src/main/kotlin/de/hglabor/plugins/hungergames/game/phase/IngamePhase.kt
+++ src/main/kotlin/de/hglabor/plugins/hungergames/game/phase/IngamePhase.kt
@@ -1,4 +1,50 @@
 package de.hglabor.plugins.hungergames.game.phase
 
-class IngamePhase {
+import de.hglabor.plugins.hungergames.Prefix
+import de.hglabor.plugins.hungergames.game.GameManager
+import de.hglabor.plugins.hungergames.game.mechanics.DeathMessages
+import de.hglabor.plugins.hungergames.game.phase.phases.InvincibilityPhase
+import de.hglabor.plugins.hungergames.game.phase.phases.PvPPhase
+import de.hglabor.plugins.hungergames.player.PlayerStatus
+import de.hglabor.plugins.hungergames.player.hgPlayer
+import org.bukkit.GameMode
+import org.bukkit.event.EventHandler
+import org.bukkit.event.entity.PlayerDeathEvent
+import org.bukkit.event.player.PlayerJoinEvent
+
+open class IngamePhase(maxDuration: Long, nextPhase: GamePhase): GamePhase(maxDuration, nextPhase) {
+    override fun getTimeString(): String = ""
+
+    @EventHandler
+    fun onPlayerDeath(event: PlayerDeathEvent) {
+        val player = event.entity
+        player.hgPlayer.status = PlayerStatus.ELIMINATED
+        player.spigot().respawn()
+        player.gameMode = GameMode.SPECTATOR
+        DeathMessages.announce(event)
+
+        if (player.killer != null) {
+            val killer = player.killer ?: return
+            killer.hgPlayer.kills.inc()
+        }
+    }
+
+    @EventHandler
+    fun onPlayerJoin(event: PlayerJoinEvent) {
+        val player = event.player
+        val hgPlayer = player.hgPlayer
+        if (GameManager.phase == InvincibilityPhase) {
+            if (hgPlayer.status == PlayerStatus.LOBBY) {
+                hgPlayer.login()
+                hgPlayer.makeGameReady()
+                player.sendMessage("${Prefix}The game has already started. You should hurry now!")
+            }
+        } else if (GameManager.phase == PvPPhase) {
+            if (hgPlayer.status == PlayerStatus.LOBBY) {
+                hgPlayer.status = PlayerStatus.SPECTATOR
+                player.sendMessage("${Prefix}The game has already started.")
+                player.gameMode = GameMode.SPECTATOR
+            }
+        }
+    }
 }
