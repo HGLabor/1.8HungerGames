88c748d4c7fa3f028347c34a642feaccaf2c161c
BestAutoyes <enrico-eifert@t-online.de>
2022-04-04 00:48:37 +0200

    put lapis in the enchanter when opening it

--- src/main/kotlin/de/hglabor/plugins/hungergames/game/mechanics/LapisInEnchanter.kt
+++ src/main/kotlin/de/hglabor/plugins/hungergames/game/mechanics/LapisInEnchanter.kt
@@ -2,6 +2,9 @@ package de.hglabor.plugins.hungergames.game.mechanics
 
 import net.axay.kspigot.event.listen
 import org.bukkit.DyeColor
+import org.bukkit.Material
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.inventory.InventoryCloseEvent
 import org.bukkit.event.inventory.InventoryOpenEvent
 import org.bukkit.inventory.EnchantingInventory
 import org.bukkit.material.Dye
@@ -12,6 +15,17 @@ object LapisInEnchanter {
         listen<InventoryOpenEvent> {
             if (it.inventory !is EnchantingInventory) return@listen
             it.inventory.setItem(1, Dye().apply { color = DyeColor.BLUE }.toItemStack(64))
+        }
+
+        listen<InventoryClickEvent> {
+            if (it.clickedInventory !is EnchantingInventory) return@listen
+            if (it.currentItem.type != Material.INK_SACK) return@listen
+            it.isCancelled = true
+        }
+
+        listen<InventoryCloseEvent> {
+            if (it.inventory !is EnchantingInventory) return@listen
+            it.inventory.setItem(1, null)
         }
     }
 }
