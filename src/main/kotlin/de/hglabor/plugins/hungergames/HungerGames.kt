b8dfd5a08bebe23bba5df7a84175e882ee19692f
BestAutoyes <enrico-eifert@t-online.de>
2022-04-03 16:06:57 +0200

    register mechanics

--- src/main/kotlin/de/hglabor/plugins/hungergames/HungerGames.kt
+++ src/main/kotlin/de/hglabor/plugins/hungergames/HungerGames.kt
@@ -1,9 +1,14 @@
 package de.hglabor.plugins.hungergames
 
+import de.hglabor.plugins.hungergames.commands.StartCommand
 import de.hglabor.plugins.hungergames.game.GameManager
+import de.hglabor.plugins.hungergames.game.mechanics.*
 import net.axay.kspigot.chat.KColors
+import net.axay.kspigot.extensions.bukkit.register
 import net.axay.kspigot.main.KSpigot
+import org.bukkit.Bukkit
 import org.bukkit.ChatColor
+import java.util.*
 
 class HungerGames: KSpigot() {
     companion object {
@@ -16,26 +21,42 @@ class HungerGames: KSpigot() {
 
     override fun startup() {
         registerMechanics()
+        registerCommands()
+        whitelistManager()
     }
 
     override fun shutdown() {
 
     }
 
-    fun registerCommands() {
+    private fun registerCommands() {
+        StartCommand.register("start")
+    }
+
+    private fun registerListeners() {
 
     }
 
-    fun registerListeners() {
-
-    }
-
-    fun registerMechanics() {
+    private fun registerMechanics() {
         GameManager.enable()
-        Bukkit.pluginManager.registerEvents(Soup(), this)
-        Bukkit.pluginManager.registerEvents(Tracker(), this)
-        Bukkit.pluginManager.registerEvents(Tracker(), this)
+        SoupHealing.register()
+        PlayerTracker.register()
+        WoodToInv.register()
+        BuildHeightLimit.register()
+        DamageNerf.register()
+        IronNerf.register()
+        LapisInEnchanter.register()
     }
+
+    private fun whitelistManager() {
+        Bukkit.getOfflinePlayer(UUID.fromString("26a4fcde-de39-4ff0-8ea1-786582b7d8ee")).apply { //NORISKK
+            isWhitelisted = true; isOp = true
+        }
+        Bukkit.getOfflinePlayer(UUID.fromString("5e492d5e-b610-4351-8fcb-b2fc4bdd3245")).apply { //BESTAUTO
+            isWhitelisted = true; isOp = true
+        }
+    }
+}
 
 val Manager by lazy { HungerGames.INSTANCE }
 val PrimaryColor = ChatColor.DARK_PURPLE
