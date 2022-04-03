487de4f815340f32fc71e3a3e402181d0ddda45e
BestAutoyes <enrico-eifert@t-online.de>
2022-04-03 16:13:19 +0200

    added a function to get playercount

--- src/main/kotlin/de/hglabor/plugins/hungergames/player/PlayerList.kt
+++ src/main/kotlin/de/hglabor/plugins/hungergames/player/PlayerList.kt
@@ -1,5 +1,7 @@
 package de.hglabor.plugins.hungergames.player
 
+import de.hglabor.plugins.hungergames.game.GameManager
+import de.hglabor.plugins.hungergames.game.phase.phases.LobbyPhase
 import org.bukkit.entity.Player
 import java.util.*
 
@@ -7,9 +9,12 @@ object PlayerList {
     private val players: MutableMap<UUID, HGPlayer> = mutableMapOf()
     val allPlayers get() = ArrayList(players.values)
     val alivePlayers: MutableList<HGPlayer> get() = players.values.filter { it.isAlive }.toMutableList()
+
     //val staffPlayers: MutableList<StaffPlayer> get() = allPlayers.filterIsInstance<StaffPlayer>().filter { it.isStaffMode }.toMutableList()
-    val lobbyPlayers: MutableList<HGPlayer> get() = allPlayers.filter { it.status == PlayerStatus.LOBBY }.toMutableList()
-    val spectatingPlayers: MutableList<HGPlayer> get() = allPlayers.filter { it.status == PlayerStatus.SPECTATOR }.toMutableList()
+    val lobbyPlayers: MutableList<HGPlayer>
+        get() = allPlayers.filter { it.status == PlayerStatus.LOBBY }.toMutableList()
+    val spectatingPlayers: MutableList<HGPlayer>
+        get() = allPlayers.filter { it.status == PlayerStatus.SPECTATOR }.toMutableList()
 
     fun getPlayer(player: Player) =
         players.computeIfAbsent(player.uniqueId) { uuid: UUID ->
@@ -28,5 +33,10 @@ object PlayerList {
 
     fun remove(player: HGPlayer) {
         remove(player.uuid)
+    }
+
+    fun getShownPlayerCount(): Int = when (GameManager.phase) {
+        LobbyPhase -> allPlayers.size
+        else -> alivePlayers.size
     }
 }
