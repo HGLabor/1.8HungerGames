6a2315866cd76c062d0082c54e372ef79020d917
BestAutoyes <enrico-eifert@t-online.de>
2022-04-03 16:12:49 +0200

    added a playertracker

---
+++ src/main/kotlin/de/hglabor/plugins/hungergames/game/mechanics/PlayerTracker.kt
@@ -0,0 +1,39 @@
+package de.hglabor.plugins.hungergames.game.mechanics
+
+import de.hglabor.plugins.hungergames.Prefix
+import de.hglabor.plugins.hungergames.player.hgPlayer
+import net.axay.kspigot.chat.KColors
+import net.axay.kspigot.event.listen
+import org.bukkit.ChatColor
+import org.bukkit.Material
+import org.bukkit.entity.Player
+import org.bukkit.event.player.PlayerInteractEvent
+
+object PlayerTracker {
+    fun register() {
+        listen<PlayerInteractEvent> {
+            val player = it.player
+            if (player.itemInHand.type != Material.COMPASS) return@listen
+            val target = findTarget(player)
+
+            if (target == null) {
+                player.sendMessage("${Prefix}${ChatColor.RED}No target found.")
+                return@listen
+            }
+            player.sendMessage("${Prefix}You are now tracking ${ChatColor.LIGHT_PURPLE}${target.name}${ChatColor.GRAY}.")
+            player.compassTarget = target.location
+        }
+    }
+
+    private fun findTarget(trackingPlayer: Player): Player? =
+        trackingPlayer.world.players.asSequence().filterIsInstance<Player>()
+            .filter { other -> other != trackingPlayer && other.hgPlayer.isAlive }
+            .map { other -> other.location.distanceSquared(trackingPlayer.location) to other }
+            .sortedBy { other ->
+                other.first
+            }.firstOrNull() { other ->
+                other.first > 30.0
+            }?.second
+
+}
+
