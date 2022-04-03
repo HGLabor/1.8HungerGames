73e0e31735b221227798e416a7e531f5e5f197a8
BestAutoyes <enrico-eifert@t-online.de>
2022-04-03 16:08:33 +0200

    make players ready when game is starting

--- src/main/kotlin/de/hglabor/plugins/hungergames/game/phase/phases/InvincibilityPhase.kt
+++ src/main/kotlin/de/hglabor/plugins/hungergames/game/phase/phases/InvincibilityPhase.kt
@@ -1,14 +1,23 @@
 package de.hglabor.plugins.hungergames.game.phase.phases
 
-import de.hglabor.plugins.hungergames.game.phase.GamePhase
+import de.hglabor.plugins.hungergames.game.GameManager
+import de.hglabor.plugins.hungergames.game.phase.IngamePhase
+import de.hglabor.plugins.hungergames.player.HGPlayer
+import de.hglabor.plugins.hungergames.player.PlayerList
 import net.axay.kspigot.extensions.broadcast
+import org.bukkit.event.EventHandler
+import org.bukkit.event.entity.EntityDamageEvent
 
-object InvincibilityPhase: GamePhase(120, PvPPhase) {
+object InvincibilityPhase: IngamePhase(120, PvPPhase) {
     override fun onStart() {
         broadcast("InvincibilityPhase")
+        PlayerList.allPlayers.forEach(HGPlayer::makeGameReady)
     }
 
-    override fun getTimeString(): String {
-        TODO("Not yet implemented")
+    override fun getTimeString(): String = "Invinc: ${maxDuration - GameManager.elapsedTime.get()}"
+
+    @EventHandler
+    fun onDamage(event: EntityDamageEvent) {
+        event.isCancelled = true
     }
 }
