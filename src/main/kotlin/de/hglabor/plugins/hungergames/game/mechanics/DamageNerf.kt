3b529525cc3e4af05773172990ab06c267c55628
BestAutoyes <enrico-eifert@t-online.de>
2022-04-03 16:02:24 +0200

    added damagenerf

---
+++ src/main/kotlin/de/hglabor/plugins/hungergames/game/mechanics/DamageNerf.kt
@@ -0,0 +1,16 @@
+package de.hglabor.plugins.hungergames.game.mechanics
+
+import net.axay.kspigot.event.listen
+import org.bukkit.entity.Player
+import org.bukkit.event.entity.EntityDamageByEntityEvent
+
+object DamageNerf {
+    fun register() {
+        listen<EntityDamageByEntityEvent> {
+            if (!(it.damager is Player && it.entity is Player)) return@listen
+            val itemName = (it.damager as Player).itemInHand.type.name.lowercase()
+            if (itemName.endsWith("_sword") || itemName.endsWith("_axe"))
+                it.damage *= 0.7
+        }
+    }
+}
