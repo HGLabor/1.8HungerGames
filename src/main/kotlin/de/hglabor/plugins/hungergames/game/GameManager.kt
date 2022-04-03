1d5da077e2309f4dc6c1f490d34ef4193924aa73
BestAutoyes <enrico-eifert@t-online.de>
2022-04-03 16:05:08 +0200

    moved phase task to gamemanager

--- src/main/kotlin/de/hglabor/plugins/hungergames/game/GameManager.kt
+++ src/main/kotlin/de/hglabor/plugins/hungergames/game/GameManager.kt
@@ -1,24 +1,59 @@
 package de.hglabor.plugins.hungergames.game
 
 import de.hglabor.plugins.hungergames.Manager
+import de.hglabor.plugins.hungergames.game.mechanics.feast.Feast
 import de.hglabor.plugins.hungergames.game.phase.GamePhase
 import de.hglabor.plugins.hungergames.game.phase.phases.LobbyPhase
+import de.hglabor.plugins.hungergames.player.hgPlayer
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import net.axay.kspigot.event.listen
+import net.axay.kspigot.runnables.sync
+import net.axay.kspigot.runnables.task
 import org.bukkit.Bukkit
 import org.bukkit.event.HandlerList
+import org.bukkit.event.player.PlayerJoinEvent
+import java.util.concurrent.atomic.AtomicLong
 
 object GameManager {
+    val world = Bukkit.getWorld("world")
     var phase: GamePhase = LobbyPhase
+    var elapsedTime: AtomicLong = AtomicLong(0)
+    var feast: Feast? = null
 
     fun enable() {
         phase.start()
-        phase.tick()
+        Bukkit.getPluginManager().registerEvents(phase, Manager)
+        world.worldBorder.size = 600.0*2
+
+        listen<PlayerJoinEvent> { it.player.hgPlayer.login() }
+
+        runBlocking {
+            launch {
+                startTimer()
+            }
+        }
+        task(true, 20, 20) { phase.tick(phase.tickCount++) }
     }
 
     fun startNextPhase() {
-        HandlerList.unregisterAll(phase);
+        HandlerList.unregisterAll(phase)
         val newPhase = phase.nextPhase ?: return
         phase = newPhase
-        Bukkit.getPluginManager().registerEvents(phase, Manager);
+        Bukkit.getPluginManager().registerEvents(phase, Manager)
         phase.start()
+        elapsedTime.set(0)
+    }
+
+    private suspend fun startTimer() {
+        task(false, 20, 20) {
+            if (elapsedTime.get() == phase.maxDuration) {
+                sync {
+                    startNextPhase()
+                }
+                return@task
+            }
+            phase.incrementElapsedTime()
+        }
     }
 }
--- src/main/kotlin/de/hglabor/plugins/hungergames/game/phase/GamePhase.kt
+++ src/main/kotlin/de/hglabor/plugins/hungergames/game/phase/GamePhase.kt
@@ -1,47 +1,22 @@
 package de.hglabor.plugins.hungergames.game.phase
 
 import de.hglabor.plugins.hungergames.game.GameManager
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import net.axay.kspigot.runnables.sync
-import net.axay.kspigot.runnables.task
 import org.bukkit.event.Listener
 
-abstract class GamePhase(val maxDuration: Long, val nextPhase: GamePhase?): Listener {
-    var elapsedTime: Long = 0
+abstract class GamePhase(val maxDuration: Long, val nextPhase: GamePhase?) : Listener {
+    var tickCount: Int = 0
 
     open fun onStart() {}
 
-    open fun tick() {}
+    open fun tick(tickCount: Int) {}
 
     abstract fun getTimeString(): String
 
     fun start() {
         onStart()
-
-        elapsedTime = 0
-        runBlocking {
-            launch {
-                startTimer()
-            }
-        }
-    }
-
-    private suspend fun startTimer() {
-        task(false, 20, 20, maxDuration, true, endCallback = {
-            sync {
-                GameManager.startNextPhase()
-            }
-        }) {
-            if (elapsedTime == maxDuration) {
-                it.cancel()
-                return@task
-            }
-            incrementElapsedTime()
-        }
     }
 
     open fun incrementElapsedTime() {
-        elapsedTime++
+        GameManager.elapsedTime.incrementAndGet()
     }
 }
