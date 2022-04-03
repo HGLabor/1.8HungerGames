792cf1fecb0cd17f1a98ca04952228609e4c0e98
BestAutoyes <enrico-eifert@t-online.de>
2022-04-03 16:11:03 +0200

    wood will be added to the inventory of the player who is breaking it

---
+++ src/main/kotlin/de/hglabor/plugins/hungergames/game/mechanics/WoodToInv.kt
@@ -0,0 +1,21 @@
+package de.hglabor.plugins.hungergames.game.mechanics
+
+import net.axay.kspigot.event.listen
+import org.bukkit.Material
+import org.bukkit.event.block.BlockBreakEvent
+
+object WoodToInv {
+    fun register() {
+        listen<BlockBreakEvent> {
+            if (it.isCancelled) return@listen
+            if (it.block.type == Material.LOG || it.block.type == Material.LOG_2) {
+                val player = it.player
+                if (player.inventory.contents.any { it == null || it.type == Material.AIR }) {
+                    player.inventory.addItem(*it.block.drops.toTypedArray())
+                    it.isCancelled = true
+                    it.block.type = Material.AIR
+                }
+            }
+        }
+    }
+}
