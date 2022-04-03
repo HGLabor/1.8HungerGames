713c6a097940862b8104f0b45be5540d6d75962e
BestAutoyes <enrico-eifert@t-online.de>
2022-04-04 01:15:23 +0200

    THIS NEED TO BE FIXED

--- src/main/kotlin/de/hglabor/plugins/kitapi/player/PlayerKits.kt
+++ src/main/kotlin/de/hglabor/plugins/kitapi/player/PlayerKits.kt
@@ -1,11 +1,15 @@
 package de.hglabor.plugins.kitapi.player
 
 import de.hglabor.plugins.hungergames.Prefix
+import de.hglabor.plugins.hungergames.game.GameManager
+import de.hglabor.plugins.hungergames.game.phase.GamePhase
+import de.hglabor.plugins.hungergames.game.phase.phases.LobbyPhase
 import de.hglabor.plugins.hungergames.player.hgPlayer
 import de.hglabor.plugins.kitapi.kit.ClickableKitItem
 import de.hglabor.plugins.kitapi.kit.Kit
 import de.hglabor.plugins.kitapi.kit.isKitItem
 import net.axay.kspigot.event.listen
+import net.axay.kspigot.extensions.broadcast
 import org.bukkit.ChatColor
 import org.bukkit.entity.Player
 import org.bukkit.event.player.PlayerInteractEvent
@@ -19,12 +23,19 @@ object PlayerKits {
             val item = event.item ?: return@listen
             val hgPlayer = event.player.hgPlayer
             val playerKit = hgPlayer.kit
+            // TODO FIX REQUIRED
+            broadcast("1")
             if (item.isKitItem) {
+                broadcast("is kit item")
                 val kitKey = item.itemMeta.displayName.split("${ChatColor.DARK_PURPLE}").last()
+                broadcast(kitKey)
                 if (playerKit.properties.kitname != kitKey) return@listen
+                broadcast("2")
                 val kitItem = playerKit.internal.items.toList().first { it.second.stack == item }.second
-                if (kitItem is ClickableKitItem)
+                if (kitItem is ClickableKitItem) {
+                    broadcast("3")
                     kitItem.onClick.invoke(event)
+                }
             }
         }
     }
@@ -40,5 +51,7 @@ object PlayerKits {
     fun Player.chooseKit(kit: Kit<*>) {
         hgPlayer.kit = kit
         sendMessage("${Prefix}You chose the kit ${ChatColor.LIGHT_PURPLE}${kit.properties.kitname}${ChatColor.GRAY}.")
+        if (GameManager.phase != LobbyPhase)
+            kit.internal.givePlayer(this)
     }
 }
