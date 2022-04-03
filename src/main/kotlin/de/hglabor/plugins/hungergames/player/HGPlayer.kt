a3bf3d0f42a9c744032b40691375c7ad519f6790
BestAutoyes <enrico-eifert@t-online.de>
2022-04-03 16:06:38 +0200

    make a player gameready when joining the game

--- src/main/kotlin/de/hglabor/plugins/hungergames/player/HGPlayer.kt
+++ src/main/kotlin/de/hglabor/plugins/hungergames/player/HGPlayer.kt
@@ -1,8 +1,16 @@
 package de.hglabor.plugins.hungergames.player
 
+import de.hglabor.plugins.hungergames.game.GameManager
 import de.hglabor.plugins.hungergames.scoreboard.Board
+import de.hglabor.plugins.hungergames.scoreboard.setScoreboard
+import net.axay.kspigot.extensions.bukkit.feedSaturate
+import net.axay.kspigot.extensions.bukkit.heal
 import org.bukkit.Bukkit
+import org.bukkit.ChatColor
+import org.bukkit.GameMode
+import org.bukkit.Material
 import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
 import java.util.*
 
 open class HGPlayer(val uuid: UUID, val name: String) {
@@ -12,6 +20,7 @@ open class HGPlayer(val uuid: UUID, val name: String) {
     val isAlive: Boolean
         get() = status == PlayerStatus.INGAME || status == PlayerStatus.OFFLINE
     var status: PlayerStatus = PlayerStatus.LOBBY
+
     //TODO var combatLogMob: UUID? = null
     var offlineTime: Int = 120
     //var hasBeenRevived: Boolean = false
@@ -20,6 +29,38 @@ open class HGPlayer(val uuid: UUID, val name: String) {
     var isInCombat = false
 
     var board: Board? = null
+
+    fun login() {
+        val player = bukkitPlayer ?: return
+        if (board != null) {
+            board!!.setScoreboard(player)
+            return
+        }
+
+        fun kills(): Int = kills
+
+        board = player.setScoreboard {
+            title = "${ChatColor.DARK_PURPLE}HGLabor"
+            period = 20
+            content {
+                +{ GameManager.phase.getTimeString() }
+                +{ "Kills: ${kills()}" }
+                +{ "Spieler: ${PlayerList.getShownPlayerCount()}" }
+                +{ "Kit: Gar keins" }
+            }
+        }
+    }
+
+    fun makeGameReady() {
+        status = PlayerStatus.INGAME
+        bukkitPlayer?.apply {
+            inventory.clear()
+            inventory.addItem(ItemStack(Material.COMPASS))
+            gameMode = GameMode.SURVIVAL
+            feedSaturate()
+            heal()
+        }
+    }
 }
 
 val Player.hgPlayer get() = PlayerList.getPlayer(this)
