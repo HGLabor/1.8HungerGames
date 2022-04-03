73e4de5ceaef11a85d0e34cb9beb2cfde4cff239
BestAutoyes <enrico-eifert@t-online.de>
2022-04-03 16:09:48 +0200

    added events to lobbyphase

--- src/main/kotlin/de/hglabor/plugins/hungergames/game/phase/phases/LobbyPhase.kt
+++ src/main/kotlin/de/hglabor/plugins/hungergames/game/phase/phases/LobbyPhase.kt
@@ -3,39 +3,54 @@ package de.hglabor.plugins.hungergames.game.phase.phases
 import de.hglabor.plugins.hungergames.game.GameManager
 import de.hglabor.plugins.hungergames.game.phase.GamePhase
 import de.hglabor.plugins.hungergames.player.PlayerList
-import de.hglabor.plugins.hungergames.scoreboard.setScoreboard
 import net.axay.kspigot.extensions.broadcast
 import org.bukkit.event.EventHandler
+import org.bukkit.event.block.BlockBreakEvent
+import org.bukkit.event.block.BlockPlaceEvent
+import org.bukkit.event.entity.EntityDamageEvent
+import org.bukkit.event.player.PlayerDropItemEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
 
-object LobbyPhase: GamePhase(120, InvincibilityPhase) {
+object LobbyPhase : GamePhase(120, InvincibilityPhase) {
     override fun onStart() {
         broadcast("Lobby!")
     }
 
-    override fun tick() {
+    override fun getTimeString(): String = "Starting: ${maxDuration - GameManager.elapsedTime.get()}"
 
-    }
-
-    override fun getTimeString(): String {
-        TODO("Not yet implemented")
-    }
 
     override fun incrementElapsedTime() {
-        //TODO if (PlayerList.) >= 2
-        elapsedTime++
+        if (PlayerList.allPlayers.size >= 2) GameManager.elapsedTime.getAndIncrement()
     }
 
     @EventHandler
     fun onPlayerJoin(event: PlayerJoinEvent) {
-        event.joinMessage = null
         PlayerList.getPlayer(event.player)
     }
 
     @EventHandler
     fun onPlayerQuit(event: PlayerQuitEvent) {
-        event.quitMessage = null
         PlayerList.remove(event.player.uniqueId)
+    }
+
+    @EventHandler
+    fun onBlockBreak(event: BlockBreakEvent) {
+        event.isCancelled = true
+    }
+
+    @EventHandler
+    fun onBlockPlace(event: BlockPlaceEvent) {
+        event.isCancelled = true
+    }
+
+    @EventHandler
+    fun onDamage(event: EntityDamageEvent) {
+        event.isCancelled = true
+    }
+
+    @EventHandler
+    fun onDropItem(event: PlayerDropItemEvent) {
+        event.isCancelled = true
     }
 }
