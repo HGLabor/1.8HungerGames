a0bbe229795623d5908a17ea3dbd1c041268ef6c
BestAutoyes <enrico-eifert@t-online.de>
2022-04-04 21:44:59 +0200

    fixed kititem

--- src/main/kotlin/de/hglabor/plugins/kitapi/kit/KitItem.kt
+++ src/main/kotlin/de/hglabor/plugins/kitapi/kit/KitItem.kt
@@ -1,14 +1,11 @@
 package de.hglabor.plugins.kitapi.kit
 
+import org.bukkit.ChatColor
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.inventory.ItemStack
 
 sealed class KitItem {
     abstract val stack: ItemStack
-
-    init {
-        kitItems[stack] = this
-    }
 }
 
 class SimpleKitItem(override val stack: ItemStack) : KitItem()
@@ -18,7 +15,5 @@ class ClickableKitItem(
     val onClick: (PlayerInteractEvent) -> Unit,
 ) : KitItem()
 
-private val kitItems = HashMap<ItemStack, KitItem>()
-
 val ItemStack.isKitItem: Boolean
-    get() = kitItems.contains(this)
+    get() = itemMeta.lore.first() == "${ChatColor.DARK_PURPLE}Kititem"
