6a92d3de9f1572c1a2adc2be0cf0356fb41e006d
BestAutoyes <enrico-eifert@t-online.de>
2022-04-06 19:39:34 +0200

    ups

--- src/main/kotlin/de/hglabor/plugins/hungergames/game/phase/phases/PvPPhase.kt
+++ src/main/kotlin/de/hglabor/plugins/hungergames/game/phase/phases/PvPPhase.kt
@@ -16,7 +16,7 @@ import org.bukkit.ChatColor
 object PvPPhase : IngamePhase(1800, EndPhase) {
     private val recraftInspector: RecraftInspector = RecraftInspector()
     override val timeName = "Ingame${ChatColor.DARK_GRAY}"
-    override fun getTimeString() = TimeConverter.stringify(remainingTime.toInt())
+    override fun getTimeString() = TimeConverter.stringify((GameManager.elapsedTime.get()).toInt())
 
     override fun tick(tickCount: Int) {
         // recraft nerf
