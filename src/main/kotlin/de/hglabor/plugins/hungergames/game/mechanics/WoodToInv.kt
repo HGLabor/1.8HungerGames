4ec22f1a2ffd4e79dc933ad4fcd26cb0b729ef31
mooziii <63669478+mooziii@users.noreply.github.com>
2022-04-06 20:53:49 +0200

    Name shadowing :grimacing:

--- src/main/kotlin/de/hglabor/plugins/hungergames/game/mechanics/WoodToInv.kt
+++ src/main/kotlin/de/hglabor/plugins/hungergames/game/mechanics/WoodToInv.kt
@@ -10,7 +10,7 @@ object WoodToInv {
             if (it.isCancelled) return@listen
             if (it.block.type == Material.LOG || it.block.type == Material.LOG_2) {
                 val player = it.player
-                if (player.inventory.contents.any { it == null || it.type == Material.AIR }) {
+                if (player.inventory.contents.any { block ->  block == null || block.type == Material.AIR }) {
                     player.inventory.addItem(*it.block.drops.toTypedArray())
                     it.isCancelled = true
                     it.block.type = Material.AIR
