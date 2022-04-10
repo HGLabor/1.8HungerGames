4ab8f7ae41dd4d0ceef5021576941db8da3314b2
BestAutoyes <enrico-eifert@t-online.de>
2022-04-10 20:23:15 +0200

    ????

--- src/main/kotlin/de/hglabor/plugins/kitapi/implementation/Viking.kt
+++ src/main/kotlin/de/hglabor/plugins/kitapi/implementation/Viking.kt
@@ -1,15 +1,10 @@
 package de.hglabor.plugins.kitapi.implementation
 
-import de.hglabor.plugins.hungergames.player.PlayerList.getPlayer
 import de.hglabor.plugins.kitapi.kit.Kit
 import de.hglabor.plugins.kitapi.kit.KitProperties
 import org.bukkit.Material
 import org.bukkit.entity.Player
 import org.bukkit.event.entity.EntityDamageByEntityEvent
-import org.bukkit.event.entity.EntityDamageEvent
-import org.bukkit.inventory.ItemFlag
-import org.bukkit.inventory.ItemStack
-
 
 class VikingProperties : KitProperties()
 
@@ -17,10 +12,9 @@ val Viking = Kit("Viking", ::VikingProperties) {
     displayMaterial = Material.IRON_AXE
 
     kitPlayerEvent<EntityDamageByEntityEvent>({ it.entity as? Player }) { it, player ->
-
-    when (player.itemInHand.type) {
-        Material.DIAMOND_AXE, Material.WOOD_AXE, Material.IRON_AXE, Material.STONE_AXE
-            -> it.damage += 1.5
-        else -> Unit
+        when (player.itemInHand.type) {
+            Material.DIAMOND_AXE, Material.WOOD_AXE, Material.IRON_AXE, Material.STONE_AXE ->
+                it.damage += 1.5
+        }
     }
-}}
+}
