950e3db271a6bdd7a8ee874ced89530983e3f425
BestAutoyes <enrico-eifert@t-online.de>
2022-04-13 03:35:39 +0200

    safe the timestamp when the cooldown will expire

--- src/main/kotlin/de/hglabor/plugins/kitapi/cooldown/CooldownManager.kt
+++ src/main/kotlin/de/hglabor/plugins/kitapi/cooldown/CooldownManager.kt
@@ -12,14 +12,14 @@ import java.util.*
 object CooldownManager {
     private val cooldownCoroutineScope = CoroutineScope(Dispatchers.IO)
 
-    private val cooldownMap = HashMap<Cooldown, MutableSet<UUID>>()
+    private val cooldownMap = HashMap<Cooldown, MutableMap<UUID, Long>>()
 
     fun addCooldown(cooldown: Cooldown, player: Player) {
         val uuid = player.uniqueId
         val cooldownCollection = cooldownMap.getOrPut(cooldown) {
-            Collections.synchronizedSet(HashSet())
+            Collections.synchronizedMap(HashMap())
         }
-        cooldownCollection += uuid
+        cooldownCollection += uuid to System.currentTimeMillis() + cooldown.property.get()
         cooldownCoroutineScope.launch {
             delay(cooldown.property.get())
             cooldownCollection -= uuid
@@ -36,4 +36,15 @@ object CooldownManager {
 
     fun hasCooldown(cooldown: Cooldown, player: Player) =
         cooldownMap[cooldown]?.contains(player.uniqueId) == true
+
+    fun getRemainingCooldown(cooldown: Cooldown, player: Player): String {
+        if (!hasCooldown(cooldown, player)) return "Ready"
+        val restMillis = (cooldownMap[cooldown]?.get(player.uniqueId) ?: 0) - System.currentTimeMillis()
+        if (restMillis <= 0) return "Ready"
+        val seconds = (restMillis/1000).toInt()
+        // can't get the scoreboard to update more frequent than once ~a second :<
+        /*val miliseconds = (restMillis%1000).toInt() / 100
+        return  String.format("%02d:%02d", seconds, miliseconds)*/
+        return  String.format("%02ds", seconds)
+    }
 }
