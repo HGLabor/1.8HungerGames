19623e394b44f08ca5a4e26e44202350a6012032
BestAutoyes <enrico-eifert@t-online.de>
2022-04-14 21:46:57 +0200

    updated revive

--- src/main/kotlin/de/hglabor/plugins/kitapi/implementation/Revive.kt
+++ src/main/kotlin/de/hglabor/plugins/kitapi/implementation/Revive.kt
@@ -3,12 +3,19 @@ package de.hglabor.plugins.kitapi.implementation
 import de.hglabor.plugins.kitapi.cooldown.CooldownProperties
 import de.hglabor.plugins.kitapi.cooldown.applyCooldown
 import de.hglabor.plugins.kitapi.kit.Kit
+import net.axay.kspigot.extensions.geometry.add
+import net.axay.kspigot.runnables.taskRunLater
 import org.bukkit.Bukkit
+import org.bukkit.ChatColor
 import org.bukkit.Effect
 import org.bukkit.Material
 import org.bukkit.Sound
 import org.bukkit.entity.Player
 import org.bukkit.event.entity.EntityDamageEvent
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
+import kotlin.math.cos
+import kotlin.math.sin
 
 
 class ReviveProperties : CooldownProperties(50000)
@@ -20,15 +27,26 @@ val Revive = Kit("Revive", ::ReviveProperties) {
         if (player.health - it.finalDamage <= 0.0) {
             applyCooldown(player) {
                 it.isCancelled = true
+                player.addPotionEffects(
+                    listOf(
+                        PotionEffect(PotionEffectType.REGENERATION, 10, 9),
+                        PotionEffect(PotionEffectType.ABSORPTION, 100, 1),
+                        PotionEffect(PotionEffectType.FIRE_RESISTANCE, 800, 0)
+                    )
+                )
+                taskRunLater(10) {
+                    PotionEffect(PotionEffectType.REGENERATION, 790, 2)
+                }
+
                 val scaleX = 1
                 val scaleY = 1
                 val density = 1.0
                 var i = 0.0
+                player.world.playSound(player.location, Sound.ANVIL_BREAK, 3f, 1f)
                 while (i < 2 * Math.PI) {
-                    val x = Math.cos(i) * scaleX
-                    val y = Math.sin(i) * scaleY
-                    player.playEffect(player.location, Effect.LARGE_SMOKE, 15)
-                    player.playSound(player.location, Sound.ANVIL_BREAK, 3f, 1f)
+                    val x = cos(i) * scaleX
+                    val y = sin(i) * scaleY
+                    player.world.playEffect(player.location.clone().add(x, 0, y), Effect.HAPPY_VILLAGER, 15)
                     i += density
                 }
             }
