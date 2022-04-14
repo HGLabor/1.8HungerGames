a24ed7b764d814d5b4fe39b5b7da79730b694d17
Tobi van Qoch <75244203+TobivanQoch@users.noreply.github.com>
2022-04-14 20:04:49 +0200

    Update Revive.kt

--- src/main/kotlin/de/hglabor/plugins/kitapi/implementation/Revive.kt
+++ src/main/kotlin/de/hglabor/plugins/kitapi/implementation/Revive.kt
@@ -3,13 +3,12 @@ package de.hglabor.plugins.kitapi.implementation
 import de.hglabor.plugins.kitapi.cooldown.CooldownProperties
 import de.hglabor.plugins.kitapi.cooldown.applyCooldown
 import de.hglabor.plugins.kitapi.kit.Kit
-import net.axay.kspigot.runnables.taskRunLater
+import org.bukkit.Bukkit
 import org.bukkit.Effect
 import org.bukkit.Material
+import org.bukkit.Sound
 import org.bukkit.entity.Player
 import org.bukkit.event.entity.EntityDamageEvent
-import org.bukkit.potion.PotionEffect
-import org.bukkit.potion.PotionEffectType
 
 
 class ReviveProperties : CooldownProperties(50000)
@@ -21,24 +20,18 @@ val Revive = Kit("Revive", ::ReviveProperties) {
         if (player.health - it.finalDamage <= 0.0) {
             applyCooldown(player) {
                 it.isCancelled = true
-                // TODO cool animation or sound?
-                player.addPotionEffects(
-                    listOf(
-                        PotionEffect(PotionEffectType.ABSORPTION, 100, 2),
-                        PotionEffect(PotionEffectType.REGENERATION, 10, 4),
-                        PotionEffect(PotionEffectType.FIRE_RESISTANCE, 800, 0)
-                    )
-                )
-                taskRunLater(10) {
-                    PotionEffect(PotionEffectType.REGENERATION, 900, 2)
+                val scaleX = 1
+                val scaleY = 1
+                val density = 1.0
+                var i = 0.0
+                while (i < 2 * Math.PI) {
+                    val x = Math.cos(i) * scaleX
+                    val y = Math.sin(i) * scaleY
+                    player.playEffect(player.location, Effect.LARGE_SMOKE, 15)
+                    player.playSound(player.location, Sound.ANVIL_BREAK, 3f, 1f)
+                    i += density
                 }
             }
         }
     }
 }
-
-
-
-
-
-
