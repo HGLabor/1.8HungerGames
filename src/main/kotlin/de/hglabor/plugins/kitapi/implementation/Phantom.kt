e6b2991966c3248b8f16ed86a0176519616db5e5
BestAutoyes <enrico-eifert@t-online.de>
2022-04-15 23:13:26 +0200

    announce that a phantom has risen

--- src/main/kotlin/de/hglabor/plugins/kitapi/implementation/Phantom.kt
+++ src/main/kotlin/de/hglabor/plugins/kitapi/implementation/Phantom.kt
@@ -6,12 +6,14 @@ import de.hglabor.plugins.hungergames.utils.cancelFalldamage
 import de.hglabor.plugins.kitapi.cooldown.CooldownProperties
 import de.hglabor.plugins.kitapi.cooldown.applyCooldown
 import de.hglabor.plugins.kitapi.kit.Kit
+import net.axay.kspigot.extensions.broadcast
 import net.axay.kspigot.items.itemStack
 import net.axay.kspigot.items.meta
 import net.axay.kspigot.items.name
 import net.axay.kspigot.runnables.task
 import org.bukkit.ChatColor
 import org.bukkit.Material
+import org.bukkit.util.Vector
 import java.util.concurrent.atomic.AtomicInteger
 
 class PhantomProperties : CooldownProperties(30000) {
@@ -27,6 +29,9 @@ val Phantom = Kit("Phantom", ::PhantomProperties) {
             it.player.apply {
                 allowFlight = true
                 isFlying = true
+                player.sendMessage("${Prefix}You are now able to fly.")
+                player.velocity = Vector(0.0, 0.3, 0.0)
+                broadcast("${ChatColor.BOLD}A ${SecondaryColor}${ChatColor.BOLD}Phantom ${ChatColor.WHITE}${ChatColor.BOLD}has risen!")
 
                 val timer = AtomicInteger(kit.properties.flightTime)
                 task(true, 20, 20) { task ->
