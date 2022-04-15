1b961e2528efd09e78d2eb7d8c1522bf7ad96d65
BestAutoyes <enrico-eifert@t-online.de>
2022-04-16 00:44:42 +0200

    changed toblerone

--- src/main/kotlin/de/hglabor/plugins/kitapi/implementation/TobleroneKit.kt
+++ src/main/kotlin/de/hglabor/plugins/kitapi/implementation/TobleroneKit.kt
@@ -1,51 +1,52 @@
 package de.hglabor.plugins.kitapi.implementation
 
-import de.hglabor.plugins.hungergames.Prefix
+import de.hglabor.plugins.kitapi.cooldown.CooldownProperties
+import de.hglabor.plugins.kitapi.cooldown.applyCooldown
 import de.hglabor.plugins.kitapi.kit.Kit
-import de.hglabor.plugins.kitapi.kit.KitProperties
-import net.axay.kspigot.chat.KColors
-import org.bukkit.DyeColor
+import net.axay.kspigot.extensions.events.isRightClick
+import org.bukkit.ChatColor
 import org.bukkit.Material
 import org.bukkit.Sound
-import org.bukkit.entity.LivingEntity
-import org.bukkit.entity.Player
-import org.bukkit.event.entity.PlayerDeathEvent
 import org.bukkit.inventory.ItemStack
 import org.bukkit.potion.PotionEffect
 import org.bukkit.potion.PotionEffectType
-import org.litote.kmongo.addEachToSet
 
-class TobleroneProperties : KitProperties() {
+class TobleroneProperties : CooldownProperties(150) {
     val effectDuration by int(60)
     val min by int(0)
-    val max by int(10)
+    val max by int(3)
 }
 
 fun rand(min: Int, max: Int): Int {
-    if(min>=max) System.out.println("${KColors.RED}THE MIN=$min IS BIGGER THAN THE MAX=$max")
-    return (Math.random() * (max - min + 1)).toInt() + min
+    if (min >= max) println("${ChatColor.RED}THE MIN=$min IS BIGGER THAN THE MAX=$max")
+    return (min..max).random()
 }
 
 val Toblerone = Kit("Toblerone", ::TobleroneProperties) {
-    displayMaterial = (Material.BANNER)
-    val uses = buildMap<Player, Int> {  }.toMutableMap()
+    displayItem = ItemStack(Material.INK_SACK, 1, 3)
 
-    kitPlayerEvent<PlayerDeathEvent> ({it.entity.killer as Player}) {it, player ->
-        val killer = it.entity.killer as? LivingEntity ?: return@kitPlayerEvent
-        if (killer !is Player) return@kitPlayerEvent
-        uses[killer]?.plus(1)
-    }
 
-    placeableItem(ItemStack(Material.BANNER)) {
+    clickableItem(ItemStack(Material.INK_SACK, 1, 3)) {
         it.isCancelled = true
-        if(uses.get(it.player)!! <= 0) { return@placeableItem  it.player.sendMessage("${Prefix}You have ${uses.get(it.player)} uses left.")}
-        else {
-            uses[it.player]?.minus(1)
-            it.player.sendMessage("${Prefix}You have ${uses.get(it.player)} uses left.")
+        if (!it.action.isRightClick) return@clickableItem
+
+        applyCooldown(it) {
+            it.player.playSound(it.player.location, Sound.EAT, 2f, 1f)
+            it.player.addPotionEffect(
+                PotionEffect(
+                    PotionEffectType.SPEED,
+                    kit.properties.effectDuration * 20,
+                    rand(kit.properties.min, kit.properties.max)
+                )
+            )
+            it.player.addPotionEffect(
+                PotionEffect(
+                    PotionEffectType.JUMP,
+                    kit.properties.effectDuration * 20,
+                    rand(kit.properties.min, kit.properties.max)
+                )
+            )
         }
-        it.player.playSound(it.player.location, Sound.EAT, 2f, 1f)
-        it.player.addPotionEffect(PotionEffect(PotionEffectType.SPEED, this.kit.properties.effectDuration * 20, rand(this.kit.properties.min, this.kit.properties.max)))
-        it.player.addPotionEffect(PotionEffect(PotionEffectType.JUMP, this.kit.properties.effectDuration * 20, rand(this.kit.properties.min, this.kit.properties.max)))
     }
 }
 
