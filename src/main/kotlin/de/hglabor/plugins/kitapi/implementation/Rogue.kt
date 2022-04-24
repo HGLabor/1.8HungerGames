f2d72aebc127679b095ab9233bf8cd75f9d971c4
BestAutoyes <71661047+BestAutoyes@users.noreply.github.com>
2022-04-24 03:54:46 +0200

    use kitproperties and added messages when dis/enabling kits

--- src/main/kotlin/de/hglabor/plugins/kitapi/implementation/Rogue.kt
+++ src/main/kotlin/de/hglabor/plugins/kitapi/implementation/Rogue.kt
@@ -1,26 +1,28 @@
 package de.hglabor.plugins.kitapi.implementation
 
-import de.hglabor.plugins.hungergames.player.HGPlayer
+import de.hglabor.plugins.hungergames.Prefix
 import de.hglabor.plugins.hungergames.player.PlayerList
-import de.hglabor.plugins.kitapi.cooldown.*
+import de.hglabor.plugins.hungergames.player.hgPlayer
+import de.hglabor.plugins.kitapi.cooldown.CooldownProperties
+import de.hglabor.plugins.kitapi.cooldown.applyCooldown
 import de.hglabor.plugins.kitapi.kit.Kit
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import net.axay.kspigot.extensions.events.isRightClick
-import org.bukkit.*
+import org.bukkit.ChatColor
+import org.bukkit.Material
 import org.bukkit.entity.Entity
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 
 class RogueProperties : CooldownProperties(16000) {
-    val distance by double(4.0)
+    val radius by double(25.0)
+    val duration by long(10000)
 }
 
 val Rogue = Kit("Rogue", ::RogueProperties) {
-    val radius = 25.0
-    val duration = 1000L * 10
     val scope = CoroutineScope(Dispatchers.IO)
 
     displayMaterial = Material.STICK
@@ -28,7 +30,7 @@ val Rogue = Kit("Rogue", ::RogueProperties) {
     clickableItem(ItemStack(Material.STICK)) {
         if (!it.action.isRightClick) return@clickableItem
         val player = it.player
-
+        val radius = kit.properties.radius
         val radiusSquared: Double = radius * radius
 
         val entities: List<Entity> = player.getNearbyEntities(radius, radius, radius) // All entities withing a box
@@ -38,15 +40,17 @@ val Rogue = Kit("Rogue", ::RogueProperties) {
                 if (entity.location.distanceSquared(player.location) > radiusSquared) continue  // All entities within a sphere
 
                 if (entity is Player) {
-                    val hgPlayer = PlayerList.getPlayer(entity.uniqueId)
+                    val hgPlayer = entity.hgPlayer
+                    if (!hgPlayer.isAlive) continue
 
-                    // hoffe das hier geht
                     scope.launch {
-                        hgPlayer?.disableKit()
+                        hgPlayer.disableKit()
+                        hgPlayer.bukkitPlayer?.sendMessage("${Prefix}Your kit has been ${ChatColor.RED}disabled${ChatColor.GRAY}.")
 
-                        delay(duration)
+                        delay(kit.properties.duration)
 
-                        hgPlayer?.enableKit()
+                        hgPlayer.enableKit()
+                        hgPlayer.bukkitPlayer?.sendMessage("${Prefix}Your kit has been ${ChatColor.GREEN}enabled${ChatColor.GRAY}.")
                     }
                 }
             }
