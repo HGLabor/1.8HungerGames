04321c19643ca9e9d4e8852e6901d8fa183847a1
Wxffel <66750759+Wxffel@users.noreply.github.com>
2022-04-26 23:21:39 +0200

    added logic

--- src/main/kotlin/de/hglabor/plugins/hungergames/game/mechanics/recraft/RecraftNerf.kt
+++ src/main/kotlin/de/hglabor/plugins/hungergames/game/mechanics/recraft/RecraftNerf.kt
@@ -1,113 +1,108 @@
 package de.hglabor.plugins.hungergames.game.mechanics.recraft
 
 import net.axay.kspigot.event.listen
+import net.axay.kspigot.extensions.broadcast
 import org.bukkit.Material
-import org.bukkit.event.player.PlayerDropItemEvent
+import org.bukkit.event.inventory.InventoryAction
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.player.PlayerPickupItemEvent
 import org.bukkit.inventory.ItemStack
 
 object RecraftNerf {
     private const val RECRAFT_LIMIT = 64
+    private val recraftList = listOf(
+        Recraft(Material.BROWN_MUSHROOM, Material.RED_MUSHROOM),
+        Recraft(Material.COCOA),
+        Recraft(Material.CACTUS)
+    )
+
+    // todo: compatibility with NoInvDropOnClose
 
     fun register() {
-        listen<PlayerDropItemEvent> { e ->
-            val recraftList = listOf(
-                Recraft(Material.BROWN_MUSHROOM, Material.RED_MUSHROOM),
-                Recraft(Material.COCOA),
-                Recraft(Material.CACTUS)
-            )
+        // todo: nur wenn items von oben in das eigene Inventar kommen und einen nerf brauchen, dann
+        listen<InventoryClickEvent> { e ->
+            broadcast("action=${e.action}")
+            broadcast("" + e.currentItem.type) // pickup
+            broadcast("" + e.cursor.type) // place
+            // if ist iwie falsch
+            if (e.clickedInventory == e.view.bottomInventory && e.currentItem.isRecraftMaterial()) {
+                broadcast("InventoryClickEvent, von oben von unten")
 
-            val contents = e.player.inventory.contents
-            contents.forEachIndexed { index, itemStack ->
-                if (itemStack == null || !itemStack.isRecraftMaterial())
-                    return@forEachIndexed
+                val eventItemStack = e.currentItem ?: return@listen
+                val recraftInfo =
+                    processRecraft(eventItemStack, e.view.bottomInventory.contents.toList()) ?: return@listen
+                val itemStack = recraftInfo.eventItemStack
 
-                val itemStackMaterial = if (itemStack.isCocoa()) Material.COCOA else itemStack.type
-                for (recraft in recraftList) {
-                    if (recraft.materials.contains(itemStackMaterial))
-                        recraft.recraftComponents.add(RecraftComponent(itemStackMaterial, itemStack.amount, index))
+                if (recraftInfo.needsNerf) {
+                    e.isCancelled = true
+                    if (itemStack.amount == 0) itemStack.type = Material.AIR
+                    e.currentItem = itemStack
+                    broadcast("stackAmount=${eventItemStack.amount}")
+                    broadcast("stackType=${eventItemStack.type}")
                 }
             }
+        }
 
-            if (recraftList.sumOf { it.soups } <= RECRAFT_LIMIT)
-                return@listen
+        // maybe: nerf every material component to rc limit
+        listen<PlayerPickupItemEvent> { e ->
+            broadcast("PlayerPickupItemEvent")
+            val eventItemStack = e.item.itemStack
+            val recraftInfo = processRecraft(eventItemStack, e.player.inventory.contents.toList()) ?: return@listen
+            val itemStack = recraftInfo.eventItemStack
 
-            val slotsToKeep = mutableListOf<Int>()
-
-            var soupsSaved = 0
-            while (soupsSaved < RECRAFT_LIMIT) {
-                val biggestRecraftStacks = recraftList.map { it.biggestRecraftStack(slotsToKeep) }
-                val biggestRecraftStack = biggestRecraftStacks.maxByOrNull { recraftStack ->
-                    if (recraftStack.isEmpty()) 0
-                    else recraftStack.minOf { it.amount }
-                } ?: break
-
-                val biggestRecraftStackSoups = biggestRecraftStack.minOf { it.amount }
-
-                if (soupsSaved + biggestRecraftStackSoups > RECRAFT_LIMIT) {
-                    val soupsToDecrease = (soupsSaved + biggestRecraftStackSoups) - RECRAFT_LIMIT
-                    val smallerRecraftComponent = biggestRecraftStack.minByOrNull { it.amount } ?: break
-                    val biggerRecraftComponent = biggestRecraftStack.maxByOrNull { it.amount } ?: break
-
-                    val smallerItemStack = contents[smallerRecraftComponent.slot]
-                    val biggerItemStack = contents[biggerRecraftComponent.slot]
-
-                    // der größere stack ist quasi mit (sich - kleineren) soups durchgekommen
-
-                    if (smallerItemStack != null && biggerItemStack != null) {
-                        smallerItemStack.amount -= soupsToDecrease
-                        e.player.inventory.setItem(smallerRecraftComponent.slot, smallerItemStack)
-                        biggerItemStack.amount = smallerItemStack.amount
-                        e.player.inventory.setItem(biggerRecraftComponent.slot, biggerItemStack)
-                    }
-
-                    for (recraftStack in biggestRecraftStack)
-                        slotsToKeep.add(recraftStack.slot)
-                    break
-                }
-
-                for (recraftStack in biggestRecraftStack)
-                    slotsToKeep.add(recraftStack.slot)
-                soupsSaved += biggestRecraftStackSoups
-            }
-
-            val recraftSlots =
-                recraftList.flatMap { recraft -> recraft.recraftComponents.map { it.slot } }
-                    .filterNot { slotsToKeep.contains(it) }
-
-            for (slot in recraftSlots) {
-                val itemStack = e.player.inventory.contents[slot]
-                itemStack.type = Material.AIR
-                e.player.inventory.setItem(slot, itemStack)
+            if (recraftInfo.needsNerf) {
+                e.isCancelled = true
+                if (itemStack.amount == 0) e.item.remove()
+                else e.item.itemStack = itemStack
+                broadcast("stackAmount=${itemStack.amount}")
+                broadcast("stackType=${itemStack.type}")
             }
         }
     }
 
-    data class RecraftComponent(val material: Material, val amount: Int, val slot: Int)
-    class Recraft(vararg _material: Material) {
-        val materials = _material.toList()
-        val recraftComponents = mutableListOf<RecraftComponent>()
-        val soups: Int
-            get() = materials.map { material -> recraftComponentsByMaterial(material).sumOf { it.amount } }.minOf { it }
+    data class RecraftInfo(val needsNerf: Boolean, val eventItemStack: ItemStack)
 
-        fun biggestRecraftStack(slotsToSkip: List<Int>): List<RecraftComponent> {
-            val biggestRecraftComponents = mutableListOf<RecraftComponent>()
-            for (material in materials) {
-                val biggestRecraftComponent =
-                    recraftComponentsByMaterial(material)
-                        .filterNot { slotsToSkip.contains(it.slot) }
-                        .maxByOrNull { it.amount } ?: break
-                biggestRecraftComponents.add(biggestRecraftComponent)
+    private fun processRecraft(eventItemStack: ItemStack, inventoryContents: List<ItemStack?>): RecraftInfo? {
+        if (!eventItemStack.isRecraftMaterial())
+            return null
+
+        val recraftComponents = mutableListOf<RecraftComponent>()
+
+        for (itemStack in inventoryContents.filterNotNull()) {
+            if (itemStack.isRecraftMaterial()) {
+                recraftComponents.add(RecraftComponent(itemStack.realMaterial(), itemStack.amount))
             }
-            return biggestRecraftComponents
         }
 
-        private fun recraftComponentsByMaterial(material: Material) =
-            recraftComponents.filter { it.material == material }
+        recraftComponents.forEach { broadcast("$it") }
+
+        recraftComponents.add(RecraftComponent(eventItemStack.realMaterial(), eventItemStack.amount))
+        val futureSoups = recraftList.sumOf { it.soups(recraftComponents) } // futureSoups
+
+        if (futureSoups > RECRAFT_LIMIT) {
+            broadcast("futureSoups=$futureSoups - RECRAFT_LIMIT=$RECRAFT_LIMIT = ${futureSoups - RECRAFT_LIMIT}")
+            broadcast("eventItemStack.amount=${eventItemStack.amount}")
+            broadcast("vermindertAmount=${eventItemStack.amount - (futureSoups - RECRAFT_LIMIT)}")
+
+            eventItemStack.amount -= futureSoups - RECRAFT_LIMIT
+            return RecraftInfo(true, eventItemStack)
+        }
+
+        return null
+    }
+
+    data class RecraftComponent(val material: Material, val amount: Int)
+    class Recraft(vararg _material: Material) {
+        private val materials = _material.toList()
+        fun soups(recraftComponents: List<RecraftComponent>) =
+            materials.map { material -> recraftComponents.filter { it.material == material }.sumOf { it.amount } }
+                .minOf { it }
     }
 
     private val recraftMaterials =
         listOf(Material.BROWN_MUSHROOM, Material.RED_MUSHROOM, Material.CACTUS, Material.COCOA)
 
-    private fun ItemStack.isCocoa() = this.type == Material.INK_SACK && this.data.data.toInt() == 3
-    private fun ItemStack.isRecraftMaterial() = recraftMaterials.contains(this.type) || this.isCocoa()
+    private fun ItemStack.isCocoa() = type == Material.INK_SACK && data.data.toInt() == 3
+    private fun ItemStack.realMaterial() = if (isCocoa()) Material.COCOA else type
+    private fun ItemStack.isRecraftMaterial() = recraftMaterials.contains(realMaterial())
 }
