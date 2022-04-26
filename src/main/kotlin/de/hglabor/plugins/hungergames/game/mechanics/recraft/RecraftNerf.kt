b62443146ef22ad185818fb69ec69a943215e17d
Wxffel <66750759+Wxffel@users.noreply.github.com>
2022-04-26 18:30:07 +0200

    renaming and started a bug fix

--- src/main/kotlin/de/hglabor/plugins/hungergames/game/mechanics/recraft/RecraftNerf.kt
+++ src/main/kotlin/de/hglabor/plugins/hungergames/game/mechanics/recraft/RecraftNerf.kt
@@ -6,7 +6,7 @@ import org.bukkit.event.player.PlayerDropItemEvent
 import org.bukkit.inventory.ItemStack
 
 object RecraftNerf {
-    private const val RECRAFT_LIMIT = 128
+    private const val RECRAFT_LIMIT = 64
 
     fun register() {
         listen<PlayerDropItemEvent> { e ->
@@ -23,58 +23,56 @@ object RecraftNerf {
 
                 val itemStackMaterial = if (itemStack.isCocoa()) Material.COCOA else itemStack.type
                 for (recraft in recraftList) {
-                    if (recraft.materials.contains(itemStackMaterial)) {
-                        val recraftComponent = RecraftComponent(itemStackMaterial, itemStack.amount, index)
-                        recraft.recraftComponents.add(recraftComponent)
-                    }
+                    if (recraft.materials.contains(itemStackMaterial))
+                        recraft.recraftComponents.add(RecraftComponent(itemStackMaterial, itemStack.amount, index))
                 }
             }
 
             if (recraftList.sumOf { it.soups } <= RECRAFT_LIMIT)
-                return@listen // no nerf needed
+                return@listen
 
-            val slotsNotToRemove = mutableListOf<Int>()
+            val slotsToKeep = mutableListOf<Int>()
 
             var soupsSaved = 0
             while (soupsSaved < RECRAFT_LIMIT) {
-                val eachRecraftsBiggestRecraftStacks = mutableListOf<List<RecraftComponent>>()
-                for (recraft in recraftList) {
-                    val biggestRecraftStacks = recraft.biggestRecraftStacks(slotsNotToRemove)
-                    eachRecraftsBiggestRecraftStacks.add(biggestRecraftStacks)
-                }
+                val biggestRecraftStacks = recraftList.map { it.biggestRecraftStack(slotsToKeep) }
+                val biggestRecraftStack = biggestRecraftStacks.maxByOrNull { recraftStack ->
+                    if (recraftStack.isEmpty()) 0
+                    else recraftStack.minOf { it.amount }
+                } ?: break
 
-                val biggestRecraftStacks = eachRecraftsBiggestRecraftStacks.maxByOrNull { biggestRecraftStacks ->
-                    if (biggestRecraftStacks.isEmpty()) 0
-                    else biggestRecraftStacks.minOf { it.amount }
-                } ?: continue
+                val biggestRecraftStackSoups = biggestRecraftStack.minOf { it.amount }
 
-                val biggestRecraftStackSoups = biggestRecraftStacks.minOf { it.amount }
                 if (soupsSaved + biggestRecraftStackSoups > RECRAFT_LIMIT) {
                     val soupsToDecrease = (soupsSaved + biggestRecraftStackSoups) - RECRAFT_LIMIT
-                    val smallerBiggestRecraftStackSlot =
-                        biggestRecraftStacks.minByOrNull { it.amount }?.slot ?: continue
-                    val itemStack = contents[smallerBiggestRecraftStackSlot]
+                    val smallerRecraftComponent = biggestRecraftStack.minByOrNull { it.amount } ?: break
+                    val biggerRecraftComponent = biggestRecraftStack.maxByOrNull { it.amount } ?: break
 
-                    if (itemStack != null) {
-                        itemStack.amount -= soupsToDecrease
-                        e.player.inventory.setItem(smallerBiggestRecraftStackSlot, itemStack)
+                    val smallerItemStack = contents[smallerRecraftComponent.slot]
+                    val biggerItemStack = contents[biggerRecraftComponent.slot]
+
+                    // der größere stack ist quasi mit (sich - kleineren) soups durchgekommen
+
+                    if (smallerItemStack != null && biggerItemStack != null) {
+                        smallerItemStack.amount -= soupsToDecrease
+                        e.player.inventory.setItem(smallerRecraftComponent.slot, smallerItemStack)
+                        biggerItemStack.amount = smallerItemStack.amount
+                        e.player.inventory.setItem(biggerRecraftComponent.slot, biggerItemStack)
                     }
 
-                    for (biggestRecraftStack in biggestRecraftStacks)
-                        slotsNotToRemove.add(biggestRecraftStack.slot)
+                    for (recraftStack in biggestRecraftStack)
+                        slotsToKeep.add(recraftStack.slot)
                     break
                 }
 
-                for (biggestRecraftStack in biggestRecraftStacks) {
-                    slotsNotToRemove.add(biggestRecraftStack.slot)
-                }
-
+                for (recraftStack in biggestRecraftStack)
+                    slotsToKeep.add(recraftStack.slot)
                 soupsSaved += biggestRecraftStackSoups
             }
 
             val recraftSlots =
-                recraftList.flatMap { recraft -> recraft.recraftComponents.map { it.slot } }.toMutableList()
-            recraftSlots.removeAll(slotsNotToRemove)
+                recraftList.flatMap { recraft -> recraft.recraftComponents.map { it.slot } }
+                    .filterNot { slotsToKeep.contains(it) }
 
             for (slot in recraftSlots) {
                 val itemStack = e.player.inventory.contents[slot]
@@ -85,27 +83,19 @@ object RecraftNerf {
     }
 
     data class RecraftComponent(val material: Material, val amount: Int, val slot: Int)
-
     class Recraft(vararg _material: Material) {
         val materials = _material.toList()
         val recraftComponents = mutableListOf<RecraftComponent>()
         val soups: Int
-            get() {
-                val componentAmounts = hashMapOf<Material, Int>()
-                for (material in materials) {
-                    val recraftComponentAmount = recraftComponentsByMaterial(material).sumOf { it.amount }
-                    componentAmounts[material] = recraftComponentAmount
-                }
+            get() = materials.map { material -> recraftComponentsByMaterial(material).sumOf { it.amount } }.minOf { it }
 
-                return componentAmounts.minOf { it.value }
-            }
-
-        fun biggestRecraftStacks(slotsToSkip: List<Int>): List<RecraftComponent> {
+        fun biggestRecraftStack(slotsToSkip: List<Int>): List<RecraftComponent> {
             val biggestRecraftComponents = mutableListOf<RecraftComponent>()
             for (material in materials) {
                 val biggestRecraftComponent =
-                    recraftComponentsByMaterial(material).filter { !slotsToSkip.contains(it.slot) }
-                        .maxByOrNull { it.amount } ?: continue
+                    recraftComponentsByMaterial(material)
+                        .filterNot { slotsToSkip.contains(it.slot) }
+                        .maxByOrNull { it.amount } ?: break
                 biggestRecraftComponents.add(biggestRecraftComponent)
             }
             return biggestRecraftComponents
@@ -115,7 +105,9 @@ object RecraftNerf {
             recraftComponents.filter { it.material == material }
     }
 
-    private val recraftMaterials = listOf(Material.BROWN_MUSHROOM, Material.RED_MUSHROOM, Material.CACTUS)
+    private val recraftMaterials =
+        listOf(Material.BROWN_MUSHROOM, Material.RED_MUSHROOM, Material.CACTUS, Material.COCOA)
+
     private fun ItemStack.isCocoa() = this.type == Material.INK_SACK && this.data.data.toInt() == 3
     private fun ItemStack.isRecraftMaterial() = recraftMaterials.contains(this.type) || this.isCocoa()
 }
