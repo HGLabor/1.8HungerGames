77142526f0368ac13b14d1c860ae0162075b22b5
enrico <noreply@enricoe.de>
2023-09-21 02:41:40 +0200

    only show info about arena if its enabled

--- src/main/kotlin/de/hglabor/plugins/hungergames/commands/InfoCommand.kt
+++ src/main/kotlin/de/hglabor/plugins/hungergames/commands/InfoCommand.kt
@@ -2,6 +2,7 @@ package de.hglabor.plugins.hungergames.commands
 
 import de.hglabor.plugins.hungergames.game.GameManager
 import de.hglabor.plugins.hungergames.game.mechanics.implementation.arena.Arena
+import de.hglabor.plugins.hungergames.game.mechanics.implementation.arena.ArenaMechanic
 import de.hglabor.plugins.hungergames.player.PlayerList
 import org.bukkit.ChatColor
 import org.bukkit.command.Command
@@ -17,10 +18,12 @@ object InfoCommand : CommandExecutor {
     ): Boolean {
         sender.sendMessage("${ChatColor.RED}${ChatColor.BOLD}${GameManager.phase.timeName}: ${ChatColor.WHITE}${GameManager.phase.getTimeString()}")
         sender.sendMessage("${ChatColor.GREEN}${ChatColor.BOLD}Players: ${ChatColor.WHITE}${PlayerList.getShownPlayerCount()}")
-        sender.sendMessage("")
-        sender.sendMessage("${ChatColor.RED}${ChatColor.BOLD}Arena: ${ChatColor.WHITE}${if (Arena.isOpen) "Open" else "Closed"}")
-        sender.sendMessage("${ChatColor.RED}${ChatColor.BOLD}Waiting: ${ChatColor.WHITE}${Arena.queuedPlayers.size}")
-        sender.sendMessage("${ChatColor.RED}${ChatColor.BOLD}Fighting: ${ChatColor.WHITE}${Arena.currentMatch?.players?.size ?: 0}")
+        if (ArenaMechanic.internal.isEnabled) {
+            sender.sendMessage("")
+            sender.sendMessage("${ChatColor.RED}${ChatColor.BOLD}Arena: ${ChatColor.WHITE}${if (Arena.isOpen) "Open" else "Closed"}")
+            sender.sendMessage("${ChatColor.RED}${ChatColor.BOLD}Waiting: ${ChatColor.WHITE}${Arena.queuedPlayers.size}")
+            sender.sendMessage("${ChatColor.RED}${ChatColor.BOLD}Fighting: ${ChatColor.WHITE}${Arena.currentMatch?.players?.size ?: 0}")
+        }
         return true
     }
 }
