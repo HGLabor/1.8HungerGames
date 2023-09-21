fc5d92e05fac22354836964f33309d8b74114a4e
enrico <noreply@enricoe.de>
2023-09-21 02:23:58 +0200

    revamp arena

--- src/main/kotlin/de/hglabor/plugins/hungergames/game/mechanics/MechanicsManager.kt
+++ src/main/kotlin/de/hglabor/plugins/hungergames/game/mechanics/MechanicsManager.kt
@@ -1,9 +1,11 @@
 package de.hglabor.plugins.hungergames.game.mechanics
 
 import de.hglabor.plugins.hungergames.game.mechanics.implementation.*
+import de.hglabor.plugins.hungergames.game.mechanics.implementation.arena.ArenaMechanic
 
 object MechanicsManager {
     val mechanics = listOf(
+        ArenaMechanic,
         BuildHeightLimit,
         DamageNerf,
         OreNerf,
