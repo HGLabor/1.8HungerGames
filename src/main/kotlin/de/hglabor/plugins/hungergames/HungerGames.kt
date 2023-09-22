c3319d20d9f3c9a5bdba1097da3bb75013b66ba8
enrico <noreply@enricoe.de>
2023-09-23 00:36:28 +0200

    rmeove whitelist stuff

--- src/main/kotlin/de/hglabor/plugins/hungergames/HungerGames.kt
+++ src/main/kotlin/de/hglabor/plugins/hungergames/HungerGames.kt
@@ -4,14 +4,15 @@ import de.hglabor.plugins.hungergames.commands.*
 import de.hglabor.plugins.hungergames.game.GameManager
 import de.hglabor.plugins.hungergames.game.mechanics.MechanicsGUI
 import de.hglabor.plugins.hungergames.game.mechanics.MechanicsManager
-import de.hglabor.plugins.hungergames.game.mechanics.implementation.*
+import de.hglabor.plugins.hungergames.game.mechanics.implementation.KitSelector
+import de.hglabor.plugins.hungergames.game.mechanics.implementation.PlayerTracker
+import de.hglabor.plugins.hungergames.game.mechanics.implementation.RecraftRecipes
+import de.hglabor.plugins.hungergames.game.mechanics.implementation.SoupHealing
 import net.axay.kspigot.extensions.bukkit.register
 import net.axay.kspigot.main.KSpigot
-import org.bukkit.Bukkit
 import org.bukkit.ChatColor
 import org.bukkit.WorldCreator
 import java.io.File
-import java.util.*
 
 class HungerGames : KSpigot() {
     companion object {
@@ -30,7 +31,6 @@ class HungerGames : KSpigot() {
         this.server.createWorld(WorldCreator("arena"))
         registerMechanics()
         registerCommands()
-        whitelistManager()
     }
 
     override fun shutdown() {
@@ -61,21 +61,6 @@ class HungerGames : KSpigot() {
         KitSelector.register()
         RecraftRecipes.register()
         MechanicsGUI(MechanicsManager.mechanics).register()
-    }
-
-    private fun whitelistManager() {
-        Bukkit.getOfflinePlayer(UUID.fromString("26a4fcde-de39-4ff0-8ea1-786582b7d8ee")).apply { //NORISKK
-            isWhitelisted = true; isOp = true
-        }
-        Bukkit.getOfflinePlayer(UUID.fromString("5e492d5e-b610-4351-8fcb-b2fc4bdd3245")).apply { //BESTAUTO
-            isWhitelisted = true; isOp = true
-        }
-        Bukkit.getOfflinePlayer(UUID.fromString("e4ccbe7c-45ef-4194-b645-851f2002de89")).apply { //MOOZIII
-            isWhitelisted = true; isOp = true
-        }
-        Bukkit.getOfflinePlayer(UUID.fromString("50bf6931-e149-4743-9210-92cd58d85c5d")).apply { //TAITO
-            isWhitelisted = true; isOp = true
-        }
     }
 }
 
